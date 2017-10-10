7e8adc271ee56d5ef1023c8329a8cf3792c6f698
Ozee31 <flavien@commit42.fr>
2017-10-10 10:56:58 +0200

    activate cookie_restriction and personalize message - refs #10744

---
+++ htdocs/app/code/local/Jumbobag/Core/sql/jumbobag_core_setup/upgrade-1.0.1-1.0.2.php
@@ -0,0 +1,25 @@
+<?php
+$this->startSetup();
+
+try {
+    // Activate cookie_restriction
+    $item = [
+        'path' => 'web/cookie/cookie_restriction',
+        'value' => 1,
+        'scope' => 'default',
+        'scope_id' => 0,
+    ];
+
+    $setup = new Mage_Core_Model_Config();
+    $setup->saveConfig($item['path'], $item['value'], $item['scope'], $item['scope_id']);
+
+    // Add translation to cookie_restriction_notice_block
+    $block = Mage::getModel('cms/block')->load(2)->addData([
+        'content' => '{{block type="core/template" template="cms/cookie.phtml"}}'
+    ]);
+    $block->save();
+} catch (Exception $e) {
+    Mage::logException($e);
+}
+
+$this->endSetup();
