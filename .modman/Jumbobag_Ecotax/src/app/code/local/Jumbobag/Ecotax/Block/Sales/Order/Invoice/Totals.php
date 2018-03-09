842bd3b8c408e93b4665c31c93666abea2362756
Julien Pradet <julien@occitech.fr>
2018-03-09 17:07:30 +0100

    Display ecotax on client invoices - refs #11035

---
+++ .modman/Jumbobag_Ecotax/src/app/code/local/Jumbobag/Ecotax/Block/Sales/Order/Invoice/Totals.php
@@ -0,0 +1,37 @@
+<?php
+
+class Jumbobag_Ecotax_Block_Sales_Order_Invoice_Totals extends Mage_Sales_Block_Order_Invoice_Totals
+{
+    protected function _initTotals()
+    {
+        parent::_initTotals();
+
+        $ecotaxTotalQuote = new Jumbobag_Ecotax_Model_Total_Ecotax_Quote();
+        $ecotaxTotal = $this->getEcotaxTotal($this->getOrder()->getAllVisibleItems());
+
+        if ($ecotaxTotal > 0) {
+            $this->addTotalBefore(new Varien_Object([
+                'code' => $ecotaxTotalQuote->getCode(),
+                'value' => $ecotaxTotal,
+                'base_value' => $ecotaxTotal,
+                'label' => $ecotaxTotalQuote->getLabel()
+            ], 'shipping'));
+        }
+
+        return $this;
+    }
+
+    public function getEcotaxTotal($items)
+    {
+        $ecotaxTotal = 0;
+        foreach ($items as $item) {
+            /**
+             * @var $item Mage_Sales_Model_Order_Item
+             */
+            $ecotax = $item->getEcotax();
+            $qty = $item->getQtyOrdered();
+            $ecotaxTotal += $ecotax * $qty;
+        }
+        return $ecotaxTotal;
+    }
+}
