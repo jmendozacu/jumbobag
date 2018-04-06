7693f73fe28b9fcf76f744f11a0ba6e827383447
Giovanni Olivera <giovanni@commit42.fr>
2018-04-06 12:44:14 +0200

    Remove parallax - refs #11104

--- .modman/Jumbobag_Core/src/skin/frontend/jumbobag/default/js/produit-min.js
+++ .modman/Jumbobag_Core/src/skin/frontend/jumbobag/default/js/produit-min.js
@@ -1,1 +1,39 @@
-!function($){jQuery(document).ready(function(){$(".fullscreen-before").css("background-image","url("+$("#product-img").html()+")"),$("#product-name-and-price").detach().prependTo($(".height-centered")),$(".presentation-produit").css("background-image","url("+$("#fond-pres-produit").html()+")"),$(".entretien").css("background-image","url("+$("#fond-entretien").html()+")"),$(".fullscreen-before .en-savoir-plus").click(function(){$("html, body").animate({scrollTop:$("#page_start").offset().top},500)});var e=25,t=e/$(window).height(),r=e/$(window).width();$(".fullscreen-before").mousemove(function(e){var o=e.pageX-$(window).width()/2,n=e.pageY-$(window).height()/2,i=r*o*-1-25,c=t*n*-1-50;$(".fullscreen-before").css("background-position",i+"px     "+c+"px")}),$("#product-options-wrapper label.required").html("Couleurs :"),$(".swatches-container .swatch-img").click(function(){var e=$(this).attr("title");$("#shopper_gallery_carousel li").each(function(){$(this).attr("data-label")===e&&$(".product-images .product-image").html($(this).html())})})}),jQuery(window).load(function(){$(".fullscreen-before").height($(window).height())})}(jQuery);
+!(function($) {
+  jQuery(document).ready(function() {
+    $(".fullscreen-before").css(
+      "background-image",
+      "url(" + $("#product-img").html() + ")"
+    ),
+      $("#product-name-and-price")
+        .detach()
+        .prependTo($(".height-centered")),
+      $(".presentation-produit").css(
+        "background-image",
+        "url(" + $("#fond-pres-produit").html() + ")"
+      ),
+      $(".entretien").css(
+        "background-image",
+        "url(" + $("#fond-entretien").html() + ")"
+      ),
+      $(".fullscreen-before .en-savoir-plus").click(function() {
+        $("html, body").animate(
+          { scrollTop: $("#page_start").offset().top },
+          500
+        );
+      });
+    var e = 25,
+      t = e / $(window).height(),
+      r = e / $(window).width();
+    $("#product-options-wrapper label.required").html("Couleurs :"),
+      $(".swatches-container .swatch-img").click(function() {
+        var e = $(this).attr("title");
+        $("#shopper_gallery_carousel li").each(function() {
+          $(this).attr("data-label") === e &&
+            $(".product-images .product-image").html($(this).html());
+        });
+      });
+  }),
+    jQuery(window).load(function() {
+      $(".fullscreen-before").height($(window).height());
+    });
+})(jQuery);
