d1be6a1226bb0a009a558805517ec3098e7a65df
Pof Magicfingers <pof@podshows.fr>
2019-05-06 15:24:05 +0200

    Update deployment tasks

--- .c42/deploy.rb
+++ .c42/deploy.rb
@@ -1,5 +1,5 @@
 ['HttpAuth', 'Composer', 'Magento', 'Modman'].each do |plugin|
-	load ".c42/deploy/recipes/occitech/#{plugin}-Recipe.rb"
+  load ".c42/deploy/recipes/occitech/#{plugin}-Recipe.rb"
 end
 
 set :app_shared_dirs, (app_shared_dirs | ["/feeds"])
@@ -10,30 +10,30 @@ set :default_stage, "preprod"
 server "occitech3.alwaysdata.net", :app, :web, :db, :primary => true
 
 task :preprod do
-	set :deploy_to, "/home/jbag/deployment/preprod"
-	set :branch, "develop"
-	set :webhost, "https://dev.jumbobag.fr"
-		
-	set :http_auth_users, [["demo", "demo2017"]]
-	after "deploy:finalize_update", "httpAuth:protect"
-	after "jumbobag:finalize_update" do
-		run "rm -rf #{latest_release}/htdocs/app/etc/modules/LMB_EDI.xml #{latest_release}/htdocs/app/code/local/LMB"
-	end
+  set :deploy_to, "/home/jbag/deployment/preprod"
+  set :branch, "develop"
+  set :webhost, "https://dev.jumbobag.fr"
+
+  set :http_auth_users, [["demo", "demo2017"]]
+  after "deploy:finalize_update", "httpAuth:protect"
+  after "jumbobag:finalize_update" do
+    run "rm -rf #{latest_release}/htdocs/app/etc/modules/LMB_EDI.xml #{latest_release}/htdocs/app/code/local/LMB"
+  end
 end
 
 task :production do
-	set :deploy_to, "/home/jbag/deployment/prod"
-	set :branch, "master"
-	set :webhost, "https://www.jumbobag.fr"
+  set :deploy_to, "/home/jbag/deployment/prod"
+  set :branch, "master"
+  set :webhost, "https://www.jumbobag.fr"
 end
 
 # see https://github.com/capistrano/capistrano/blob/master/lib/capistrano/ext/multistage.rb#L22
 on :load do
-	if stages.include?(ARGV.first)
-		find_and_execute_task(ARGV.first) if ARGV.any?{ |option| option =~ /-T|--tasks|-e|--explain/ }
-	else
-		find_and_execute_task(default_stage) if exists?(:default_stage)
-	end
+  if stages.include?(ARGV.first)
+    find_and_execute_task(ARGV.first) if ARGV.any?{ |option| option =~ /-T|--tasks|-e|--explain/ }
+  else
+    find_and_execute_task(default_stage) if exists?(:default_stage)
+  end
 end
 
 set :application, "jumbobag"
@@ -54,7 +54,7 @@ set :http_auth_path, app_path
 set :composer_options, "--verbose --prefer-dist --no-dev"
 
 before "deploy:create_symlink" do
-	run "touch #{latest_release}#{app_path}maintenance.flag"
+  run "touch #{latest_release}#{app_path}maintenance.flag"
 end
 
 before "mage:finalize_update", "jumbobag:finalize_update"
@@ -64,21 +64,24 @@ after "deploy", "deploy:cleanup"
 after "deploy", "mage:cc"
 
 namespace :jumbobag do
-	desc 'Update content in the shared directory from the repository'
-	task :finalize_update do
-		composer.install
-		modman.deploy_all
-		run "cp #{File.join(latest_release, app_path, ".htaccess-dist")} #{File.join(latest_release, app_path, ".htaccess")}"
-	end
-	
-	task :restart_process do
-		run "pkill php-cgi"
-		run "curl -s -o /dev/null -w \"%{http_code}\" #{webhost}; true"
-	end
-	
-	after "deploy", "jumbobag:restart_process"
+  desc 'Update content in the shared directory from the repository'
+  task :finalize_update do
+    composer.install
+    run %{rm -rf "#{latest_release}/.modman/magneto-debug"}
+
+    modman.deploy_all
+
+    run "cp #{File.join(latest_release, app_path, ".htaccess-dist")} #{File.join(latest_release, app_path, ".htaccess")}"
+  end
+
+  task :restart_process do
+    run "pkill php-cgi"
+    run "curl -s -o /dev/null -w \"%{http_code}\" #{webhost}; true"
+  end
+
+  after "deploy", "jumbobag:restart_process"
 end  
 
 after "deploy" do
-	run "echo #{latest_revision} > #{File.join(latest_release, app_path, 'rev.txt')}"
+  run "echo #{latest_revision} > #{File.join(latest_release, app_path, 'rev.txt')}"
 end
--- .c42/tasks.rb
+++ .c42/tasks.rb
@@ -28,150 +28,150 @@ task "dump:get" do |date|
 end
 
 package :mage do
-	desc "run", "Run Magerun"
-	shell_task "run", MAGERUN
+  desc "run", "Run Magerun"
+  shell_task "run", MAGERUN
 
-	desc "sbase_url LOCAL_URL", "Set l'URL de Magento"
-	task "base_url" do |url|
-		url = check_url(url)
-		fatal("L'URL n'a pas au bon format") unless url
+  desc "sbase_url LOCAL_URL", "Set l'URL de Magento"
+  task "base_url" do |url|
+    url = check_url(url)
+    fatal("L'URL n'a pas au bon format") unless url
 
-		%x{
-			#{MAGERUN} config:set web/unsecure/base_url #{url}/; \
-			#{MAGERUN} config:set web/secure/base_url #{url}/
-		}
-	end
+    %x{
+            #{MAGERUN} config:set web/unsecure/base_url #{url}/; \
+    #{MAGERUN} config:set web/secure/base_url #{url}/
+    }
+  end
 
-	desc "local_xml", "Reconstruit le local.xml"
-	task "local_xml" do
-		remove_file("htdocs/app/etc/local.xml")
-		%x{
-			#{MAGERUN} local-config:generate db root root jumbobag files soadmin a0365f41e5bf39ab158608c5fef8d270;
-		}
-	end
+  desc "local_xml", "Reconstruit le local.xml"
+  task "local_xml" do
+    remove_file("htdocs/app/etc/local.xml")
+    %x{
+            #{MAGERUN} local-config:generate db root root jumbobag files soadmin a0365f41e5bf39ab158608c5fef8d270;
+        }
+    end
 
-	desc "cache:clear", "Vide cache magento"
-	shell_task "cache:clear", "#{MAGERUN} cache:flush; #{MAGERUN} cache:clean"
+    desc "cache:clear", "Vide cache magento"
+    shell_task "cache:clear", "#{MAGERUN} cache:flush; #{MAGERUN} cache:clean"
 
-	desc "cache:disable", "Vide cache magento"
-	shell_task "cache:disable", "#{MAGERUN} cache:disable"
+    desc "cache:disable", "Vide cache magento"
+    shell_task "cache:disable", "#{MAGERUN} cache:disable"
 
-	desc "symlinks", "Active les symlinks dans tout magento"
+    desc "symlinks", "Active les symlinks dans tout magento"
     shell_task "symlinks", "#{MAGERUN} dev:symlinks --global --on"
 
-	desc "demo_notice", "Demo notice"
-	shell_task "demo_notice", "#{MAGERUN} design:demo-notice --global"
+    desc "demo_notice", "Demo notice"
+    shell_task "demo_notice", "#{MAGERUN} design:demo-notice --global"
 
-	desc "flush_alerts", "Vide alerte de prix magento"
-	shell_task "flush_alerts", %{#{MAGERUN} db:query "TRUNCATE product_alert_price; TRUNCATE product_alert_stock;"}
+    desc "flush_alerts", "Vide alerte de prix magento"
+    shell_task "flush_alerts", %{#{MAGERUN} db:query "TRUNCATE product_alert_price; TRUNCATE product_alert_stock;"}
 
-	desc "disable_smtp", "Disable SMTP"
-	shell_task "disable_smtp", %{
-		#{MAGERUN} config:delete --all system/smtp/disable
-		#{MAGERUN} config:delete --all smtppro/general/option
-		#{MAGERUN} config:set system/smtp/disable 1
-		#{MAGERUN} config:set smtppro/general/option disabled
-	}
-end
-
-desc "install URL_LOCAL", "Installe le projet sur URL_LOCAL"
-task :install do
-	url = "jumbobag.test"
-
-  info("Invoking dump:get")
-  invoke "dump:get", [Time.now.strftime("%Y-%m-%d")]
-
-  sql_cat_cmd = "cat .c42/tmp/dump.sql" if File.exists?(".c42/tmp/dump.sql")
-  sql_cat_cmd = "zcat .c42/tmp/dump.sql.gz" if File.exists?(".c42/tmp/dump.sql.gz")
-  fatal("Could not find .c42/tmp/dump.sql[.gz]") unless defined?(sql_cat_cmd) && !sql_cat_cmd.nil?
-
-  unless File.exists?(".c42/docker-compose.yml") && File.exists?("docker-compose.yml")
-    info("copying docker-compose.yml")
-    copy_file("docker-compose.yml.dist", ".c42/docker-compose.yml")
-    create_link("docker-compose.yml", ".c42/docker-compose.yml")
-    if yes?("Do you want to edit docker-compose.yml? [y/N]")
-      system(%{"${EDITOR:-vim}" docker-compose.yml })
-    end
+    desc "disable_smtp", "Disable SMTP"
+    shell_task "disable_smtp", %{
+        #{MAGERUN} config:delete --all system/smtp/disable
+    #{MAGERUN} config:delete --all smtppro/general/option
+    #{MAGERUN} config:set system/smtp/disable 1
+    #{MAGERUN} config:set smtppro/general/option disabled
+    }
   end
 
-  unless File.exists?(".c42/docker/entrypoint.sh")
-    info("copying docker-compose.yml")
-    copy_file("docker/entrypoint.sh.dist", ".c42/docker/entrypoint.sh")
-    if yes?("Do you want to edit .c42/docker/entrypoint.sh? [y/N]")
-      system(%{"${EDITOR:-vim}" .c42/docker/entrypoint.sh })
+  desc "install URL_LOCAL", "Installe le projet sur URL_LOCAL"
+  task :install do
+    url = "jumbobag.test"
+
+    info("Invoking dump:get")
+    invoke "dump:get", [Time.now.strftime("%Y-%m-%d")]
+
+    sql_cat_cmd = "cat .c42/tmp/dump.sql" if File.exists?(".c42/tmp/dump.sql")
+    sql_cat_cmd = "zcat .c42/tmp/dump.sql.gz" if File.exists?(".c42/tmp/dump.sql.gz")
+    fatal("Could not find .c42/tmp/dump.sql[.gz]") unless defined?(sql_cat_cmd) && !sql_cat_cmd.nil?
+
+    unless File.exists?(".c42/docker-compose.yml") && File.exists?("docker-compose.yml")
+      info("copying docker-compose.yml")
+      copy_file("docker-compose.yml.dist", ".c42/docker-compose.yml")
+      create_link("docker-compose.yml", ".c42/docker-compose.yml")
+      if yes?("Do you want to edit docker-compose.yml? [y/N]")
+        system(%{"${EDITOR:-vim}" docker-compose.yml })
+      end
     end
-  end
 
-  info("Chmoding docker/entrypoint.sh")
-  chmod(".c42/docker/entrypoint.sh", 0755)
+    unless File.exists?(".c42/docker/entrypoint.sh")
+      info("copying docker-compose.yml")
+      copy_file("docker/entrypoint.sh.dist", ".c42/docker/entrypoint.sh")
+      if yes?("Do you want to edit .c42/docker/entrypoint.sh? [y/N]")
+        system(%{"${EDITOR:-vim}" .c42/docker/entrypoint.sh })
+      end
+    end
 
-  info("Invoking composer install")
-  invoke "composer", ["install"]
+    info("Chmoding docker/entrypoint.sh")
+    chmod(".c42/docker/entrypoint.sh", 0755)
 
-  info("Invoking docker:run")
-  invoke "docker:run", []
+    info("Invoking composer install")
+    invoke "composer", ["install"]
 
-#  info("Piping sql dump into mysql:console")
-#  run("#{sql_cat_cmd} | c42 mysql:console")
+    info("Invoking docker:run")
+    invoke "docker:run", []
 
-  info("Invoking modman:deploy")
-  invoke "modman:deploy", []
+    #  info("Piping sql dump into mysql:console")
+    #  run("#{sql_cat_cmd} | c42 mysql:console")
 
-  info("Invoking mage:local_xml")
-	invoke "mage:local_xml", []
+    info("Invoking modman:deploy")
+    invoke "modman:deploy", []
 
-	info("Invoking mage:base_url")
-	invoke "mage:base_url", [url]
+    info("Invoking mage:local_xml")
+    invoke "mage:local_xml", []
 
-	info("Invoking mage:cache:clear")
-	invoke "mage:cache:clear", []
+    info("Invoking mage:base_url")
+    invoke "mage:base_url", [url]
 
-	info("Invoking mage:cache:disable")
-	invoke "mage:cache:disable", []
+    info("Invoking mage:cache:clear")
+    invoke "mage:cache:clear", []
 
-	info("Enabling symlinks")
+    info("Invoking mage:cache:disable")
+    invoke "mage:cache:disable", []
+
+    info("Enabling symlinks")
     invoke "mage:symlinks", []
 
-	info("Invoking mage:demo_notice")
-	invoke "mage:demo_notice", []
+    info("Invoking mage:demo_notice")
+    invoke "mage:demo_notice", []
 
-	info("Invoking mage:disable_smtp")
-	invoke "mage:disable_smtp", []
-end
+    info("Invoking mage:disable_smtp")
+    invoke "mage:disable_smtp", []
+  end
 
-desc "pull", "rsync from prod server in case someone modified something there"
-task :pull do
-  exec("rsync -avzP --exclude=media --exclude=var/session --exclude=var/cache --exclude=var/log occitech-jbag:jumbobag/ htdocs/")
-end
+  desc "pull", "rsync from prod server in case someone modified something there"
+  task :pull do
+    exec("rsync -avzP --exclude=media --exclude=var/session --exclude=var/cache --exclude=var/log occitech-jbag:jumbobag/ htdocs/")
+  end
 
-package :deploy do
-	desc "preprod", "deploy to preprod"
-	task :preprod do
-	  # exec remplace le process actuel
-	  exec("bundle exec cap preprod deploy")
-	end
+  package :deploy do
+    desc "preprod", "deploy to preprod"
+    task :preprod do
+      # exec remplace le process actuel
+      exec("bundle exec cap preprod deploy")
+    end
 
-	desc "prod", "deploy (really) to prod"
-	task :prod do
-	  exec("bundle exec cap production deploy")
-	end
-end
+    desc "prod", "deploy (really) to prod"
+    task :prod do
+      exec("bundle exec cap production deploy")
+    end
+  end
 
-desc "lmb", "rsync from prod server in case someone modified LMB there"
-task :lmb do
-  exec("rsync -avzP occitech-jbag:deployment/prod/current/htdocs/app/code/local/LMB/ htdocs/app/code/local/LMB/");
-end
+  desc "lmb", "rsync from prod server in case someone modified LMB there"
+  task :lmb do
+    exec("rsync -avzP occitech-jbag:deployment/prod/current/htdocs/app/code/local/LMB/ htdocs/app/code/local/LMB/");
+  end
 
 
-private
+  private
 
-require "uri"
-def check_url(url)
-	url = "https://#{url}" unless url.respond_to?(:match) && url.match(/https?:\/\//i)
-	uri = URI.parse(url) rescue nil
-	if uri.nil? || uri.hostname.nil?
-		nil
-	else
-		uri.to_s.gsub(/\/$/, "")
-	end
-end
+  require "uri"
+  def check_url(url)
+    url = "https://#{url}" unless url.respond_to?(:match) && url.match(/https?:\/\//i)
+    uri = URI.parse(url) rescue nil
+    if uri.nil? || uri.hostname.nil?
+      nil
+    else
+      uri.to_s.gsub(/\/$/, "")
+    end
+  end
