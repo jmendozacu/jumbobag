d7faa6e95cf8994071d2688ba79616a890aae1af
Pof Magicfingers <pof@podshows.fr>
2019-05-09 14:28:39 +0200

    Add instagram animations

--- .modman/Jumbobag_Core/src/skin/frontend/jumbobag/default/js/jumbobag.js
+++ .modman/Jumbobag_Core/src/skin/frontend/jumbobag/default/js/jumbobag.js
@@ -2,9 +2,9 @@
     $(function() {
         var instaregex = /https?:\/\/(?:www.)?instagram\.com\/p\/([A-z0-9_\-]+)(?:[^\s])+/
         $(".instagram-posts").each(function() {
-            this.innerHTML = this.innerHTML.match(new RegExp(instaregex, 'g')).map(function(url) {
+            this.innerHTML = this.innerHTML.match(new RegExp(instaregex, 'g')).map(function(url, index) {
                 var id = (url.match(instaregex) || [false, false])[1];
-                return id ? "<a href='https://www.instagram.com/p/" + id + "' class='post'><img src='https://www.instagram.com/p/" + id + "/media/?size=m'></a>" : null;
+                return id ? "<a href='https://www.instagram.com/p/" + id + "' class='post wow fadeIn"+(index % 2 === 0 ? "Left" : "Right")+"'><img src='https://www.instagram.com/p/" + id + "/media/?size=m'></a>" : null;
             }).filter(Boolean).join("");
             this.classList.add("show")
         });
